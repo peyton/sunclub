f195b435c78c9d3d697c64bfd22298bd8b0e0602
peyton <id@peyton.sh>
2026-02-23 13:13:54 -0800

    Initial Commit

---
+++ sun-day/sun_dayApp.swift
@@ -0,0 +1,32 @@
+//
+//  sun_dayApp.swift
+//  sun-day
+//
+//  Created by Peyton Randolph on 2/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct sun_dayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
