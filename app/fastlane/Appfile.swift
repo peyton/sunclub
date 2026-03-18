231f5b2d399c722f2f0e9821522498fe49a0e7b0
peyton <id@peyton.sh>
2026-03-17 18:21:10 -0700

    chore: add Fastlane Swift automation

--- app/fastlane/Appfile.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-var appIdentifier: String { "app.peyton.sunclub" }
-var appleID: String { ProcessInfo.processInfo.environment["FASTLANE_APPLE_ID"] ?? "" }
--- app/fastlane/swift/Compatibility.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-typealias RunLoopMode = RunLoop.Mode
-
-extension RunLoop.Mode {
-    static let defaultRunLoopMode = RunLoop.Mode.default
-}
