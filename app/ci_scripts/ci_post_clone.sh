f8b9b1f581111ce35f5cd22a5740690e684fc499
Peyton <id@peyton.sh>
2026-03-23 01:57:25 -0700

    add

--- app/ci_scripts/ci_post_clone.sh
+++ app/ci_scripts/ci_post_clone.sh
@@ -7,4 +7,5 @@ mise install # Installs the version from .mise.toml
 
 # Runs the version of Tuist indicated in the .mise.toml file {#runs-the-version-of-tuist-indicated-in-the-misetoml-file}
 mise exec -- tuist install --path ../ # `--path` needed as this is run from within the `ci_scripts` directory
+mise exec -- tuist setup cache -p ../
 mise exec -- tuist generate -p ../ --no-open # `-p` needed as this is run from within the `ci_scripts` directory
