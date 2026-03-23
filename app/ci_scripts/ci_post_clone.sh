3d505f8078f7671f3e64f8a3741329334bbc214a
Peyton <id@peyton.sh>
2026-03-23 02:44:17 -0700

    fix: ci

--- app/ci_scripts/ci_post_clone.sh
+++ app/ci_scripts/ci_post_clone.sh
@@ -5,6 +5,6 @@ set -e
 ./bin/mise install # Installs the version from .mise.toml
 
 ./bin/mise exec -- tuist install --path ../ # `--path` needed as this is run from within the `ci_scripts` directory
-./bin/mise exec -- tuist setup insights -p ../
-./bin/mise exec -- tuist setup cache -p ../
+./bin/mise exec -- tuist setup insights
+./bin/mise exec -- tuist setup cache
 ./bin/mise exec -- tuist generate -p ../ --no-open # `-p` needed as this is run from within the `ci_scripts` directory
